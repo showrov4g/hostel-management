ccee87abff6a56672cce4efe827a1c3a10e4c9cb
showrov4g <showrovg00@gmail.com>
2025-02-18 13:21:40 +0600

    category color secctions making

--- src/components/MealByCategory.jsx
+++ src/components/MealByCategory.jsx
@@ -24,23 +24,27 @@ const MealByCategory = () => {
 
   return (
     <div className=" space-y-5 my-14">
-      <div className="my-4">
-        <ul className=" flex items-center justify-center gap-4">
+      <div className="my-4 mx-auto">
+        <ul className="flex items-center justify-center gap-2">
           {/* Sidebar content here */}
           <li>
             <button className="btn bg-[#3f8acd] hover:bg-[#6052ed] text-white"  onClick={() => setCategory("all")}>All meals</button>
           </li>
           <li>
-            <button className="btn"  onClick={() => setCategory("Breakfast")}>breakfast</button>
+            <button  className="btn bg-[#3f8acd] hover:bg-[#6052ed] text-white"   onClick={() => setCategory("Breakfast")}>breakfast</button>
           </li>
           <li>
-            <button className="btn" onClick={() => setCategory("Lunch")}>Lunch</button>
+            <button  className="btn bg-[#3f8acd] hover:bg-[#6052ed] text-white"  onClick={() => setCategory("Lunch")}>Lunch</button>
           </li>
           <li>
-            <button className="btn" onClick={() => setCategory("Dinner")}>Dinner</button>
+            <button  className="btn bg-[#3f8acd] hover:bg-[#6052ed] text-white"  onClick={() => setCategory("Dinner")}>Dinner</button>
           </li>
         </ul>
       </div> 
+      <div>
+        <h1 className="text-2xl md:text-4xl lg:text-6xl font-semibold" >Meal for <span className="bg-gradient-to-r from-[#3f8acd] to-[#6052ed] text-transparent bg-clip-text">{category}</span></h1>
+        <hr className="h-2 w-[28%] bg-gradient-to-r from-[#3f8acd] to-[#6052ed]" />
+      </div>
       <div className="flex-1 grid md:grid-cols-3 gap-4">
         {meals?.map((item) => (
           <MealCart key={item._id} item={item}></MealCart>
