b0825d128f5a48ca0289e3206c5acc2fef44fd94
showrov4g <showrovg00@gmail.com>
2025-02-19 17:15:36 +0600

    Overviews page design compleate

--- src/admin/Overview.jsx
+++ src/admin/Overview.jsx
@@ -1,7 +1,5 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "../Hooks/UseAxiosSecure";
-import { Area, AreaChart, CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 const Overview = () => {
   const axiosSecure = UseAxiosSecure();
@@ -9,46 +7,31 @@ const Overview = () => {
     queryKey: ["stats"],
     queryFn: async () => {
       const res = await axiosSecure.get("/dashboard/stats");
+      refetch();
       return res.data;
     },
   });
-  console.log(stats);
 
   return (
-    <div>
-      {/* <div>
-        <h2>Dashboard Statistics</h2>
-        <p>Total Users: {stats?.totalUsers}</p>
-        <p>Total Meals: {stats?.totalMeals}</p>
-        <p>Total Requests: {stats?.totalRequests}</p>
-        <p>Total Reviews: {stats?.totalReviews}</p>
-      </div> */}
-      <div className="stats stats-vertical lg:stats-horizontal shadow">
-        <div className="stat">
-          <div className="stat-title">Total Users</div>
-          <div className="stat-value">{stats?.totalUsers}</div>
-          
-        </div>
-
-        <div className="stat">
-          <div className="stat-title">Total Meals</div>
-          <div className="stat-value">{stats?.totalMeals}</div>
-        </div>
-
-        <div className="stat">
-          <div className="stat-title">Total Requests</div>
-          <div className="stat-value">{stats?.totalRequests}</div>
-          
-        </div>
-        <div className="stat">
-          <div className="stat-title">Total Reviews </div>
-          <div className="stat-value">{stats?.totalReviews}</div>
-          
-        </div>
+    <div className="stats stats-vertical lg:stats-horizontal shadow">
+      <div className="stat">
+        <div className="stat-title">Total Users</div>
+        <div className="stat-value">{stats?.totalUsers}</div>
       </div>
- 
 
+      <div className="stat">
+        <div className="stat-title">Total Meals</div>
+        <div className="stat-value">{stats?.totalMeals}</div>
+      </div>
 
+      <div className="stat">
+        <div className="stat-title">Total Requests</div>
+        <div className="stat-value">{stats?.totalRequests}</div>
+      </div>
+      <div className="stat">
+        <div className="stat-title">Total Reviews </div>
+        <div className="stat-value">{stats?.totalReviews}</div>
+      </div>
     </div>
   );
 };
